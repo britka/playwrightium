75e204212f521892171ada24ed81dd54eac2a9ad
britka <brit.sergey@gmail.com>
2024-04-16 12:36:02 +0300

    Add emulation for mobile devices Add different Playwrightium options Small fixes Update tests

--- src/test/java/org/brit/driver/PWDriverProvider.java
+++ src/test/java/org/brit/driver/PWDriverProvider.java
@@ -1,11 +1,14 @@
 package org.brit.driver;
 
 import com.codeborne.selenide.WebDriverProvider;
+import com.microsoft.playwright.options.Geolocation;
 import org.brit.options.PlaywrightiumOptions;
+import org.brit.permission.Permissions;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 
 public class PWDriverProvider implements WebDriverProvider {
@@ -14,7 +17,7 @@ public class PWDriverProvider implements WebDriverProvider {
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
         PlaywrightiumOptions playwrightiumOptions = new PlaywrightiumOptions();
         playwrightiumOptions.setHeadless(false);
-        playwrightiumOptions.setGeolocation(new Geolocation(46.655, 32.617));
+        playwrightiumOptions.setGeolocation(new Geolocation(46.65581, 32.6178));
         playwrightiumOptions.setPermissions(List.of(Permissions.GEOLOCATION));
         playwrightiumOptions.merge(capabilities);
         return new PlaywrightiumDriver(playwrightiumOptions);
