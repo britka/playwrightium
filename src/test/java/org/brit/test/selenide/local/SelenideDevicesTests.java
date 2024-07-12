c18e6afa47a808f0870777372510bb7f78255a24
britka <brit.sergey@gmail.com>
2024-07-12 17:04:06 +0300

    Fix playwright version in playwright.yml
    Add emulation for mobile devices
    Add different Playwrightium options
    Small fixes
    Update tests

--- src/test/java/org/brit/test/selenide/local/SelenideDevicesTests.java
+++ src/test/java/org/brit/test/selenide/local/SelenideDevicesTests.java
@@ -1,28 +1,17 @@
 package org.brit.test.selenide.local;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.conditions.webdriver.UrlCondition;
-import com.codeborne.selenide.testng.TextReport;
 import com.microsoft.playwright.options.AriaRole;
-import org.apache.commons.io.FileUtils;
 import org.brit.driver.PWDriverDeviceProvider;
 import org.brit.driver.PWDriverProvider;
 import org.brit.emulation.Device;
 import org.brit.locators.ArialSearchOptions;
 import org.brit.locators.PlaywrightiumBy;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.OutputType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import org.testng.reporters.TextReporter;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.regex.Pattern;
 
-import static com.codeborne.selenide.Condition.or;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.urlContaining;
 import static com.codeborne.selenide.WebDriverRunner.url;
@@ -34,14 +23,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
  * Created by Serhii Bryt
  * 01.04.2024 16:18
  **/
-@Listeners({TextReport.class})
 public class SelenideDevicesTests {
-
-    @AfterMethod
-    public void afterMethod(){
-        closeWebDriver();
-    }
-
     @Test
     public void deviceTest() {
         Configuration.browser = PWDriverDeviceProvider.class.getName();
@@ -57,10 +39,11 @@ public class SelenideDevicesTests {
     }
 
     @Test
-    public void geolocationTest() throws IOException {
+    public void geolocationTest(){
         Configuration.browser = PWDriverProvider.class.getName();
-        open("https://www.bing.com/maps");
-        $("button.locateMeBtn,a#LocateMeButton").click();
-        webdriver().shouldHave(urlContaining("46.655")).shouldHave(urlContaining("32.617"));
+        open("https://maps.google.com");
+        $(PlaywrightiumBy.byRole(AriaRole.BUTTON,
+                new ArialSearchOptions().setName(Pattern.compile("Your Location")))).click();
+        webdriver().shouldHave(urlContaining("46.65581")).shouldHave(urlContaining("32.6178"));
     }
 }
