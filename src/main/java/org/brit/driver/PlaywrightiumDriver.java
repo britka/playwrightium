d9a9c8ba82aefe15d5eee7644bcff4f3d7f49936
Boris Osipov <osipov.boris@gmail.com>
2024-11-14 16:02:53 +0300

    Set timeouts for browserContext() - this allows us to automatically set timeouts for new pages (tabs) opened during test execution.

--- src/main/java/org/brit/driver/PlaywrightiumDriver.java
+++ src/main/java/org/brit/driver/PlaywrightiumDriver.java
@@ -649,13 +649,16 @@ public class PlaywrightiumDriver extends RemoteWebDriver implements TakesScreens
 
         @Override
         public Timeouts implicitlyWait(long time, TimeUnit unit) {
-            page.setDefaultTimeout(Duration.of(time, unit.toChronoUnit()).toMillis());
+            long millis = Duration.of(time, unit.toChronoUnit()).toMillis();
+            page.setDefaultTimeout(millis);
+            page.context().setDefaultTimeout(millis);
             return this;
         }
 
         @Override
         public Timeouts implicitlyWait(Duration duration) {
             page.setDefaultTimeout(duration.toMillis());
+            page.context().setDefaultTimeout(duration.toMillis());
             return this;
         }
 
@@ -686,13 +689,16 @@ public class PlaywrightiumDriver extends RemoteWebDriver implements TakesScreens
 
         @Override
         public Timeouts pageLoadTimeout(long time, TimeUnit unit) {
-            page.setDefaultNavigationTimeout(Duration.of(time, unit.toChronoUnit()).toMillis());
+            long millis = Duration.of(time, unit.toChronoUnit()).toMillis();
+            page.setDefaultNavigationTimeout(millis);
+            page.context().setDefaultNavigationTimeout(millis);
             return this;
         }
 
         @Override
         public Timeouts pageLoadTimeout(Duration duration) {
             page.setDefaultNavigationTimeout(duration.toMillis());
+            page.context().setDefaultNavigationTimeout(duration.toMillis());
             return this;
         }
 
