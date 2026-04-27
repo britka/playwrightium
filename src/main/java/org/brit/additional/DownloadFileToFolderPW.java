3b10bdc749f5595ba3212c8778684b4fe5c4c2c8
Andrei Solntsev <andrei.solntsev@gmail.com>
2026-04-27 07:21:40 +0300

    update API after upgrading Selenide

--- src/main/java/org/brit/additional/DownloadFileToFolderPW.java
+++ src/main/java/org/brit/additional/DownloadFileToFolderPW.java
@@ -1,6 +1,7 @@
 package org.brit.additional;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Driver;
 import com.codeborne.selenide.files.DownloadAction;
 import com.codeborne.selenide.files.FileFilter;
 import com.codeborne.selenide.impl.DownloadFileToFolder;
@@ -11,7 +12,7 @@ import org.brit.element.PlaywrightWebElement;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -29,18 +30,19 @@ public class DownloadFileToFolderPW extends DownloadFileToFolder {
     @Override
     public File download(WebElementSource anyClickableElement, WebElement clickable, long timeout, long incrementTimeout, FileFilter fileFilter, DownloadAction action) {
         try {
-            return myDownload(anyClickableElement, (PlaywrightWebElement) clickable, timeout, incrementTimeout, fileFilter, action);
-        } catch (FileNotFoundException e) {
+            return myDownload(anyClickableElement, (PlaywrightWebElement) clickable);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public File myDownload(WebElementSource anyClickableElement, PlaywrightWebElement clickable, long timeout, long incrementTimeout, FileFilter fileFilter, DownloadAction action) throws FileNotFoundException {
+    public File myDownload(WebElementSource anyClickableElement, PlaywrightWebElement clickable) throws IOException {
         Page page = clickable.getLocator().page();
         Download download = page.waitForDownload(clickable::click);
         Path fileSaved = Paths.get(Configuration.downloadsFolder, download.suggestedFilename());
         download.saveAs(fileSaved);
-        return super.archiveFile(anyClickableElement.driver(), fileSaved.toFile());
+        Driver driver = anyClickableElement.driver();
+        return super.archiveFile(driver.config(), driver.getWebDriver(), fileSaved.toFile());
     }
 
 }
